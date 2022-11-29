ae097b9d7264455ff8929d123a5f68d90328fc99
Gautam Kumar <104510672+Developer-Gautam@users.noreply.github.com>
2022-11-29 14:57:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,15 +11,3 @@ server.listen(PORT, () =>
   console.log(`JSON Server is running on port ${PORT}`)
 );
 
-//1. npm init -y (code)
-//2. npm i json-server (code)
-//3. create server.js (file)
-//4. copy the code to server.js (today'e leacutre or json-server documentation)
-//5. node server.js 
-
-
-
-//When you divide use you will fine
-// 1. Base url 
-//2 . End point 
-// 3. Query params = 
